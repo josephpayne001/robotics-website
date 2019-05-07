02cb2917a58bba7b3e85b06c7bd030e667979102
PAYNE JOSEPH <150042069@cart.ads>
2019-05-07 15:28:25 -0700

    home site code created

---
+++ sections/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sanger High Robotics</title>
+</head>
+<body>
+<div class="navigatorl">
+    <ul>
+        <li><a href=""></a></li>
+    </ul>
+    
+</div>

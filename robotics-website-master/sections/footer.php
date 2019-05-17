b0fcb1b2dd7b28580f7eb9e6e9ce26129603f377
MOORE CAMERON <604720@cart.ads>
2019-05-17 12:54:43 -0700

    EEVVEERRYYTTHHIINNGG is one here 0w0

---
+++ robotics-website-master/sections/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <h2 class="foote">Contact us: <br>
+    Email: robots@bot.gov<br>
+    Phone: 555-555-5555<br>
+    </h2>
+    
+</footer>
+
+            
+</body>
+</html>
---
+++ robotics-website-master/sections/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sanger High Robotics</title>
+    <link rel="stylesheet" href="../main.css">
+</head>
+<body>
+<div class="headerstyle">
+
+    <h1></h1>
+
+</div>
+<nav class="topbar">
+        <ul>
+           <li><a href="?page=home">Home</a></li>
+           
+                    <li class="dd"><a href="?page=aboutUs">About</a>
+            
+            <ul class="ddc">
+            <li><a href="?page=robots">Robots</a></li>
+            <li><a href="?page=meetTheTeam">The Team</a></li>
+            </ul>
+            </li>
+            
+
+            
+            <li class="dd"><a href="?page=media">Media</a>
+            
+            <ul class="ddc">
+            <li><a href="?page=art">Art</a></li>
+            <li><a href="?page=videos">Videos</a></li>
+              <li><a href="?page=photos">Photos</a></li>
+            </ul>
+            </li>
+            <li><a href="?page=contact">Contact</a></li>
+            
+        </ul>
+    </nav>

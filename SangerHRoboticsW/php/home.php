ed7cbf28c407e165bb7ccd7b6c966896d43aa604
josephpayne001 <48033317+josephpayne001@users.noreply.github.com>
2019-05-20 14:50:12 -0700

    everything done here!

---
+++ SangerHRoboticsW/php/home.php
@@ -0,0 +1,9 @@
+<div class="home">
+<h1>H O M E</h1>
+
+<h4>Welcome to the Sanger High Robotics team website</h4>
+ 
+<p>We have video, pictures, and some art to show you what we do around the workshop, what we do at the arenas, who our members are, how some of our people spend their time when not working long hard hours on robots as well as a picture of our team.</p>
+
+<h3>Robotics team</h3>
+</div>
